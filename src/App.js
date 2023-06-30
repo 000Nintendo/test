d60ef35469ddf932257cd6f7c529c659f6d7a51a
Niten Solanki <74289995+000Nintendo@users.noreply.github.com>
2023-06-30 10:36:07 +0530

    otp send functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import Hello from "./Hello";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* 
+      <About/>
+      <Setting/> */}
 
-        <dsgbfjhgsdjfjsdh>njksdkfksdfjhkdjfkldsgit</dsgbfjhgsdjfjsdh>
-        <h2>hdfhjksdhjkhksdh
-          jfjikhsdjhfjkhsdk
-        </h2>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Hello />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
